0da105d921764e3ad827166144032c52ca418513
dines <dines@LAPTOP-PQTGQTS1>
2024-05-19 11:28:51 +0530

    Add project files.

---
+++ TASK2 DEFAULT VALUES/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        string x = default;
+        int a = default;
+        float b = default;
+        bool c = default;
+        char d = default;
+        long e = default;
+        short f = default;
+        decimal g = default;
+        Console.WriteLine("STRING       :" + x);
+        Console.WriteLine("INTEGER      :" + a);
+        Console.WriteLine("FLOAT        :" + b);
+        Console.WriteLine("BOOLEAN      :" + c);
+        Console.WriteLine("CHARECTER    :" + d);
+        Console.WriteLine("LONG INTEGER :" + e);
+        Console.WriteLine("SHORT INTEGER:" + f);
+        Console.WriteLine("DECIMAL      :" + g);
+
+
+    }
+}
